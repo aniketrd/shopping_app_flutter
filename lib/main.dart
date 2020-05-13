b4fca00dbe261a3cb23d96c052808d00d98fed34
Aniket Dhole <aniketrd@Anikets-MBP.hitronhub.home>
2020-05-13 00:03:28 -0700

    Themeing Fonts for the app

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.purple,
         accentColor: Colors.orange,
+        fontFamily: 'Lato',
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
